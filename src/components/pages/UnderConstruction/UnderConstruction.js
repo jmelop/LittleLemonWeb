25e66d68876444bd8a51e907fc3df2630d3f99a1
Juan Melo <jmelopdev@gmail.com>
2024-06-10 14:31:09 +0100

    feat: new styles in under construction component and added a icon and a diferent text

--- src/components/pages/UnderConstruction/UnderConstruction.js
+++ src/components/pages/UnderConstruction/UnderConstruction.js
@@ -1,9 +1,17 @@
+import { faHelmetSafety } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import "./UnderConstruction.css";
 
 export default function UnderConstruction() {
   return (
-    <>
-      <h2>Page under construction</h2>
-    </>
+    <section className="pagex-container">
+      <FontAwesomeIcon
+        className="icon-text-flex"
+        icon={faHelmetSafety}
+        size="3x"
+      />
+      <h2>Page under construction!</h2>
+    </section>
   );
 }
